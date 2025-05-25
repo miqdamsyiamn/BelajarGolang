44a38d37a4ad81ac697c18e6d33cb6b686af128b
miqdamsyiamn <miqdamsyiamn@gmail.com>
2025-05-25 18:30:38 +0700

    update

--- main.go
+++ main.go
@@ -2,13 +2,42 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
-	"BelajarGolang/handlers"
 )
+
+// Struct User
+type User struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
 
 func main() {
 	app := fiber.New()
 
-	app.Get("/user", handlers.GetUser)
+	app.Get("/", func(c *fiber.Ctx) error {
+	return c.SendString("Selamat datang di API BelajarGolang!")
+})
+
+
+	// Endpoint GET /user
+	app.Get("/user", func(c *fiber.Ctx) error {
+		user := User{ID: 1, Name: "Miqdam", Age: 21}
+		return c.JSON(user)
+	})
+
+	app.Post("/user", func(c *fiber.Ctx) error {
+	var user User
+
+	if err := c.BodyParser(&user); err != nil {
+		return c.Status(fiber.StatusBadRequest).SendString("Invalid JSON")
+	}
+
+	return c.JSON(fiber.Map{
+		"message": "User berhasil dibuat",
+		"data":    user,
+	})
+})
+
 
 	app.Listen(":3000")
 }
